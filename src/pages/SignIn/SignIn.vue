194ddb2633121877bc293d8c0654b4b686fc88f6
Ramil Shaibakov <rshaybakov@ptsecurity.com>
2024-02-28 22:55:05 +0500

    feat(sign-in): add page styles

--- src/pages/SignIn/SignIn.vue
+++ src/pages/SignIn/SignIn.vue
@@ -2,7 +2,66 @@
 </script>
 
 <template>
-  <section>
-    <h1>Вход</h1>
+  <section class="sign-in">
+    <h1 class="title">
+      Вход
+    </h1>
+
+    <form class="form">
+      <input
+        class="email-field"
+        type="email"
+        placeholder="Твой email"
+        required
+      >
+
+      <button
+        class="submit"
+        type="submit"
+      >
+        Получить ссылку
+      </button>
+    </form>
   </section>
 </template>
+
+<style scoped>
+.sign-in {
+  height: 100%;
+  display: grid;
+  grid-template:
+    '. . .' minmax(1rem, 1fr)
+    '. title .' min-content
+    '. form .' auto
+    '. . .' minmax(1rem, 1fr)
+    / minmax(1rem, 1fr) minmax(320px, auto) minmax(1rem, 1fr);
+}
+
+.title {
+  grid-area: title;
+}
+
+.form {
+  grid-area: form;
+  display: grid;
+  grid-template: 'email-field submit' / 320px auto;
+  gap: 1rem;
+}
+
+.email-field {
+  grid-area: email-field;
+}
+
+.submit {
+  grid-area: submit;
+}
+
+@media (width < 560px) {
+  .form {
+    grid-template:
+      'email-field'
+      'submit';
+  }
+}
+
+</style>
