0a1fa8b8b11f961d510983533e792bf32806eac2
Ramil Shaibakov <rshaybakov@ptsecurity.com>
2024-03-01 21:54:51 +0500

    chore(pinia): install & setup

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,14 @@
 import { createApp } from 'vue'
+import { createPinia } from 'pinia'
 import App from './App.vue'
 import router from './router.ts'
 import './db.ts'
 import './style.css'
 
+const pinia = createPinia()
 const app = createApp(App)
 
+app.use(pinia)
 app.use(router)
 app.mount('#app')
 
